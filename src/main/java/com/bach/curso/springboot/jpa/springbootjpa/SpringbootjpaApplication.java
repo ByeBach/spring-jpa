1376f492e428a400f284dc20a975e8224959d073
bach <byebachgm@gmail.com>
2025-05-09 17:59:11 -0300

    implementacion de metodos con distintos tipo de @Query

--- src/main/java/com/bach/curso/springboot/jpa/springbootjpa/SpringbootjpaApplication.java
+++ src/main/java/com/bach/curso/springboot/jpa/springbootjpa/SpringbootjpaApplication.java
@@ -1,6 +1,7 @@
 package com.bach.curso.springboot.jpa.springbootjpa;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,11 +23,33 @@ public class SpringbootjpaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		findOne();
+	}
+	public void list(){
 		//List<Person> persons = (List<Person>) repository.findAll();
 		//List<Person> persons = (List<Person>) repository.findByProgrammingLanguage("java");
-		List<Person> persons = (List<Person>) repository.buscarByprogrammingLanguage("Java", "Andres");
+		//List<Person> persons = (List<Person>) repository.buscarByprogrammingLanguage("Java", "Andres");
+		List<Person> persons = (List<Person>) repository.findByprogrammingLanguageAndName("Java", "Andres");
 		
 		persons.stream().forEach(person -> System.out.println(person));
+
+		List<Object[]> personValues = repository.obtenerPersonData("maria");
+
+		personValues.stream().forEach(person -> 
+			{System.out.println(person[0] + " le sabe a " + person[1]);
+		});
+	}
+	public void findOne(){
+		// Person person = null;
+		// Optional<Person> optionalPerson = repository.findById(1L);
+		// //f(!optionalPerson.isEmpty()){
+		// if(optionalPerson.isPresent()){
+		// 	person = optionalPerson.get();
+		// } 
+		// System.out.println(person);
+		
+		repository.findOneLikeName("ri").ifPresent(person -> System.out.println(person));
+
 	}
 
 }
--- src/main/java/com/bach/curso/springboot/jpa/springbootjpa/repository/PersonRepository.java
+++ src/main/java/com/bach/curso/springboot/jpa/springbootjpa/repository/PersonRepository.java
@@ -1,5 +1,6 @@
 package com.bach.curso.springboot.jpa.springbootjpa.repository;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
@@ -8,11 +9,28 @@ import org.springframework.data.repository.CrudRepository;
 import com.bach.curso.springboot.jpa.springbootjpa.entities.Person;
 
 public interface PersonRepository extends CrudRepository<Person, Long> {
-    
+
+    @Query("select p from Person p where p.id=?1")
+    Optional<Person> findOne(Long id);
+
+    @Query("select p from Person p where p.name=?1")
+    Optional<Person> findOneName(String name);
+
+    @Query("select p from Person p where p.name like %?1%")
+    Optional<Person> findOneLikeName(String name);
+
     List<Person> findByProgrammingLanguage(String programmingLanguage);
 
     @Query("SELECT p FROM Person p WHERE p.programmingLanguage=?1 and p.name=?2")
     List<Person> buscarByprogrammingLanguage(String programmingLanguage, String name);
+
+    List<Person> findByprogrammingLanguageAndName(String programmingLanguage, String name);
+
+    @Query("select p.name, p.programmingLanguage from Person p")
+    List<Object[]> obtenerPersonData();
+
+    @Query("select p.name, p.programmingLanguage from Person p where p.name=?1")
+    List<Object[]> obtenerPersonData(String name);
     
 
 }
